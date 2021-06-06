84e5b4bfe02456f923dba25c17075d8b63d035ac
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-06 03:06:58 -0400

    adding react-pro-sidebar as a dependency

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProSidebar>
+      <Menu iconShape="square">
+        <MenuItem>Dashboard</MenuItem>
+        <SubMenu title="Components">
+          <MenuItem>Component 1</MenuItem>
+          <MenuItem>Component 2</MenuItem>
+        </SubMenu>
+      </Menu>
+    </ProSidebar>
   );
 }
 
