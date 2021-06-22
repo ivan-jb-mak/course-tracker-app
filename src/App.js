615f22a0dad1e716b542cb4e006f97e3faf39667
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-22 08:35:47 -0400

    adding client side folder structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
 
-function App() {
-  return (
-    <ProSidebar>
-      <Menu iconShape="square">
-        <MenuItem>Dashboard</MenuItem>
-        <SubMenu title="Components">
-          <MenuItem>Component 1</MenuItem>
-          <MenuItem>Component 2</MenuItem>
-        </SubMenu>
-      </Menu>
-    </ProSidebar>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
