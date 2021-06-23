b55fca3f9dbf2d3d58eeb83730822a69edb5ed24
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-22 20:22:42 -0400

    adding starter code for signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,29 @@
 import React, { FC } from "react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Box,
+  Center,
+  Square,
+  Circle,
+} from "@chakra-ui/react";
 
 const Signup: FC = () => {
-  return <div></div>;
+  return (
+    <Center className="container">
+      <Box w="250px" l="600px">
+        <FormControl id="Email" isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input placeholder="Email" />
+          <FormLabel>Password</FormLabel>
+          <Input placeholder="Password" />
+        </FormControl>
+      </Box>
+    </Center>
+  );
 };
 
 export default Signup;
